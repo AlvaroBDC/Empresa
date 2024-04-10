1fe2bb2d311cb80e5d7d904194c3bcc2ae9e1f9b
Alvaro Díaz Condori <202013238h@utea.edu.pe>
2024-04-10 09:54:09 -0500

    conexion con la base de datos empresa public void abrir

---
+++ Empresa/Conexionbd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Empresa
+{
+    internal class Conexionbd
+    {
+        string cadena = "Data Source=DESKTOP-LG5K25L\\SQLEXPRESS;Initail Catalog=Empresa;Integrated Security=True";
+        public SqlConnection conectarbd = new SqlConnection();
+
+        public Conexionbd()
+        {
+            conectarbd.ConnectionString = cadena;
+        }
+        public void abrir()
+        {
+            try 
+            {
+                conectarbd.Open();
+                Console.WriteLine("Conexion abierta");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error  al abrir la BD + "ex.Message);
+            }
+        }
+    }
+}
