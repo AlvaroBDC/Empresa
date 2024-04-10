c72177695cb7a68c12c40c4a70ea2062ea1f38a7
Alvaro Díaz Condori <202013238h@utea.edu.pe>
2024-04-10 11:01:56 -0500

    nombrar los texbox y button con sus respectivas denominaciones para tenerlo ordenado

--- Empresa/Form1.Designer.cs
+++ Empresa/Form1.Designer.cs
@@ -30,24 +30,24 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtIdEmpleado = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtSueldoBruto = new System.Windows.Forms.TextBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtMontoAumento = new System.Windows.Forms.TextBox();
+            this.txtSueldoNeto = new System.Windows.Forms.TextBox();
+            this.btnCalcularBeneficios = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtCaategoria = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -76,12 +76,12 @@
             this.label2.Text = "Nombre";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // txtIdEmpleado
             // 
-            this.textBox1.Location = new System.Drawing.Point(125, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(71, 20);
-            this.textBox1.TabIndex = 4;
+            this.txtIdEmpleado.Location = new System.Drawing.Point(125, 39);
+            this.txtIdEmpleado.Name = "txtIdEmpleado";
+            this.txtIdEmpleado.Size = new System.Drawing.Size(71, 20);
+            this.txtIdEmpleado.TabIndex = 4;
             // 
             // label3
             // 
@@ -97,9 +97,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(41, 154);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.Size = new System.Drawing.Size(68, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Sueldo Buto";
+            this.label4.Text = "Sueldo Bruto";
             // 
             // label5
             // 
@@ -110,27 +110,19 @@
             this.label5.TabIndex = 7;
             this.label5.Text = "Categoria";
             // 
-            // textBox2
+            // txtNombre
             // 
-            this.textBox2.Location = new System.Drawing.Point(112, 74);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(251, 20);
-            this.textBox2.TabIndex = 8;
+            this.txtNombre.Location = new System.Drawing.Point(112, 74);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(251, 20);
+            this.txtNombre.TabIndex = 8;
             // 
-            // textBox3
+            // txtApellido
             // 
-            this.textBox3.Location = new System.Drawing.Point(112, 112);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(251, 20);
-            this.textBox3.TabIndex = 9;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(377, 154);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(52, 21);
-            this.comboBox1.TabIndex = 10;
+            this.txtApellido.Location = new System.Drawing.Point(112, 112);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(251, 20);
+            this.txtApellido.TabIndex = 9;
             // 
             // label6
             // 
@@ -141,18 +133,18 @@
             this.label6.TabIndex = 12;
             this.label6.Text = "id empleado";
             // 
-            // textBox4
+            // txtSueldoBruto
             // 
-            this.textBox4.Location = new System.Drawing.Point(134, 151);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(164, 20);
-            this.textBox4.TabIndex = 13;
+            this.txtSueldoBruto.Location = new System.Drawing.Point(134, 151);
+            this.txtSueldoBruto.Name = "txtSueldoBruto";
+            this.txtSueldoBruto.Size = new System.Drawing.Size(164, 20);
+            this.txtSueldoBruto.TabIndex = 13;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.textBox6);
-            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.btnCalcularBeneficios);
+            this.groupBox1.Controls.Add(this.txtSueldoNeto);
+            this.groupBox1.Controls.Add(this.txtMontoAumento);
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Location = new System.Drawing.Point(44, 193);
@@ -180,66 +172,73 @@
             this.label8.TabIndex = 1;
             this.label8.Text = "Sueldo Neto";
             // 
-            // textBox5
+            // txtMontoAumento
             // 
-            this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(135, 22);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(77, 20);
-            this.textBox5.TabIndex = 2;
+            this.txtMontoAumento.Enabled = false;
+            this.txtMontoAumento.Location = new System.Drawing.Point(135, 22);
+            this.txtMontoAumento.Name = "txtMontoAumento";
+            this.txtMontoAumento.Size = new System.Drawing.Size(77, 20);
+            this.txtMontoAumento.TabIndex = 2;
             // 
-            // textBox6
+            // txtSueldoNeto
             // 
-            this.textBox6.Enabled = false;
-            this.textBox6.Location = new System.Drawing.Point(290, 22);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(89, 20);
-            this.textBox6.TabIndex = 3;
+            this.txtSueldoNeto.Enabled = false;
+            this.txtSueldoNeto.Location = new System.Drawing.Point(290, 22);
+            this.txtSueldoNeto.Name = "txtSueldoNeto";
+            this.txtSueldoNeto.Size = new System.Drawing.Size(89, 20);
+            this.txtSueldoNeto.TabIndex = 3;
             // 
-            // button3
+            // btnCalcularBeneficios
             // 
-            this.button3.Location = new System.Drawing.Point(135, 56);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Calcular Beneficios";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnCalcularBeneficios.Location = new System.Drawing.Point(135, 56);
+            this.btnCalcularBeneficios.Name = "btnCalcularBeneficios";
+            this.btnCalcularBeneficios.Size = new System.Drawing.Size(116, 23);
+            this.btnCalcularBeneficios.TabIndex = 4;
+            this.btnCalcularBeneficios.Text = "Calcular Beneficios";
+            this.btnCalcularBeneficios.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnRegistrar
             // 
-            this.button1.Location = new System.Drawing.Point(86, 284);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Registrar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Location = new System.Drawing.Point(86, 284);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 11;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnLimpiar
             // 
-            this.button2.Location = new System.Drawing.Point(276, 284);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 15;
-            this.button2.Text = "Limpiar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Location = new System.Drawing.Point(276, 284);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 15;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // txtCaategoria
+            // 
+            this.txtCaategoria.Location = new System.Drawing.Point(377, 154);
+            this.txtCaategoria.Name = "txtCaategoria";
+            this.txtCaategoria.Size = new System.Drawing.Size(55, 20);
+            this.txtCaategoria.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(465, 390);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtCaategoria);
+            this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtSueldoBruto);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtIdEmpleado);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -256,24 +255,24 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtIdEmpleado;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtSueldoBruto;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSueldoNeto;
+        private System.Windows.Forms.TextBox txtMontoAumento;
+        private System.Windows.Forms.Button btnCalcularBeneficios;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtCaategoria;
     }
 }
 
